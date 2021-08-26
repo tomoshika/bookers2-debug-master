5edb83e66700b6afd16423b277aa339c111c7320
tomoshika <tmm.orange.skm@gmail.com>
2021-08-26 08:39:46 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book = Book.new
-    @user = @book.user
+    @new_book = Book.new
   end
 
   def index
@@ -37,9 +36,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
